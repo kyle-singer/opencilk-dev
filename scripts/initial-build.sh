2e03c70802f2e3d6fdf8134902527ce07bb003f1
Kyle Singer <southpaw63119@gmail.com>
2024-05-01 20:56:18 +0000

    add cheetah_config.mk; use 1/2 cores for initial build; update cheetah pointer

--- scripts/initial-build.sh
+++ scripts/initial-build.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$(dirname "${0}")/..
 
 cd ${SCRIPT_DIR}
 
-./scripts/infrastructure/tools/build $(pwd)/src/opencilk $(pwd)/build/opencilk 4
+./scripts/infrastructure/tools/build $(pwd)/src/opencilk $(pwd)/build/opencilk $(expr $(nproc) / 2)
